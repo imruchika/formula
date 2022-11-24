3b92b6f9ca2c329afaeaf28c6892ceb54d90ea40
Ruchika <ruchikabokade1612@gmail.com>
2022-11-23 22:29:27 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var formulas = document.getElementById("formulas")
+// console.log(formulas);
+var num = 1
+formulas.innerHTML = ""
+for (let i = 1; i <= 1000; i++) {
+    formulas.innerHTML += " <li>cosC - cosD = -2sin(C+D/2).sin(C-D/2</li>"
+}
